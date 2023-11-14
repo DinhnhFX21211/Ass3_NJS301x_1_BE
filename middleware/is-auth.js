98df11af3684219270d3adca7495baf94ac04b2a
DinhnhFX21211 <131755314+DinhnhFX21211@users.noreply.github.com>
2023-11-14 16:19:14 +0700

    Update is-auth.js

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -2,7 +2,8 @@ const throwError = require("../utils/ErrorHandle");
 
 module.exports = (req, res, next) => {
   if (!req.session.isLoggedIn) {
-    throwError(401, "Unauthenticated");
+     next();
+    // throwError(401, "Unauthenticated");
   }
   next();
 };
